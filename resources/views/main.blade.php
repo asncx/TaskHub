747364b3b4dc033e08c98ca31460f9982dd9cd42
asncx <1337balazs@gmail.com>
2023-08-22 00:43:58 +0200

    Improved status filtering

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -38,116 +38,120 @@
                         </div>
                     @endif
 
+                    <form action="{{ route('tasks.index') }}" method="GET" class="mb-3">
+                        <div class="form-group">
+                            <label for="status">Filter by Status:</label>
+                            <select name="status" class="form-control">
+                                <option value="all">All</option>
+                                <option value="completed">Completed</option>
+                                <option value="not_completed">Not Completed</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Filter</button>
+                    </form>
+
                     @if ($tasks->isEmpty())
-                        <!-- <h1>My Tasks</h1> <p>No tasks found.</p> -->
-                        
-                        
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
+                        <h1>My Tasks</h1>
+                        <p>No tasks found.</p>
+                </div>
+                <div class="col">
 
-                        @if (session('error'))
-                            <div class="alert alert-danger">
-                                {{ session('error') }}
-                            </div>
-                        @endif
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
 
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                    @else
-                        <form action="{{ route('tasks.index') }}" method="GET" class="mb-3">
-                            <div class="form-group">
-                                <label for="status">Filter by Status:</label>
-                                <select name="status" class="form-control">
-                                    <option value="all">All</option>
-                                    <option value="completed">Completed</option>
-                                    <option value="not_completed">Not Completed</option>
-                                </select>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Filter</button>
-                        </form>
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
 
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Description</th>
-                                    <th>Due Date</th>
-                                    <th></th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($tasks as $task)
-                                    <tr class="{{ $task->priority ? 'high-priority' : '' }}">
-                                        <td class="{{ $task->priority ? 'text-danger' : '' }}">{{ $task->description }}</td>
-                                        <td>{{ $task->due_date instanceof \Carbon\Carbon ? $task->due_date->format('Y-m-d') : \Carbon\Carbon::parse($task->due_date)->format('Y-m-d') }}</td>
-                                        <td>
-                                            <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-warning">Edit</a>
-                                        </td>
-                                        <td>
-                                            @if (!$task->completed)
-                                                <form action="{{ route('tasks.complete', $task->id) }}" method="POST">
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-success" data-task-id="{{ $task->id }}">Complete</button>
-                                                </form>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
                                 @endforeach
-                            </tbody>
-                        </table>
-                        
-                        
+                            </ul>
+                        </div>
+                    @endif
+                @else
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Due Date</th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tasks as $task)
+                                <tr class="{{ $task->priority ? 'high-priority' : '' }}">
+                                    <td class="{{ $task->priority ? 'text-danger' : '' }}">{{ $task->description }}</td>
+                                    <td>{{ $task->due_date instanceof \Carbon\Carbon ? $task->due_date->format('Y-m-d') : \Carbon\Carbon::parse($task->due_date)->format('Y-m-d') }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-warning">Edit</a>
+                                    </td>
+                                    <td>
+                                        @if (!$task->completed)
+                                            <form action="{{ route('tasks.complete', $task->id) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="btn btn-success"
+                                                    data-task-id="{{ $task->id }}">Complete</button>
+                                            </form>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
-                            {{ $tasks->links() }}
-                
+
+
+                    {{ $tasks->links() }}
+
                 </div>
                 @endif
-         
-            <div class="col">
-                <h1>Create Task</h1>
-                <form action="{{ route('tasks.store') }}" method="POST">
-                    @csrf
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea name="description" class="form-control" rows="3"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="due_date">Due Date</label>
-                        <input type="date" name="due_date" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="priority">Priority</label>
-                        <div class="form-check">
-                            <input type="hidden" name="priority" value="0"> 
-                            <input type="checkbox" name="priority" class="form-check-input" value="1"> 
-                            <label class="form-check-label" for="priority">High Priority</label>
-                        </div>
-                    </div>
-                                     
-                    <button type="submit" class="btn btn-primary">Create</button>
-                </form>                            
-            </div>
-        </div>
-    @endauth
 
-@endsection
+                <div class="col">
+                    <h1>Create Task</h1>
+                    <form action="{{ route('tasks.store') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea name="description" class="form-control" rows="3"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="due_date">Due Date</label>
+                            <input type="date" name="due_date" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="priority">Priority</label>
+                            <div class="form-check">
+                                <input type="hidden" name="priority" value="0">
+                                <input type="checkbox" name="priority" class="form-check-input" value="1">
+                                <label class="form-check-label" for="priority">High Priority</label>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </form>
+                </div>
+            </div>
+        @endauth
+
+    @endsection
 
 </div>
